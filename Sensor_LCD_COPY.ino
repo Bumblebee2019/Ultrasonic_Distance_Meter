4b30acd27745909f0ece9734384c34e26e158e48
Bumblebee2019 <57073212+Bumblebee2019@users.noreply.github.com>
2022-04-26 14:55:57 -0700

    Add files via upload

--- Sensor_LCD_COPY.ino
+++ Sensor_LCD_COPY.ino
@@ -1,7 +1,6 @@
 #include <LiquidCrystal.h>
-#include <arduino-timer.h>
 #include <string.h>
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
 
 /*
  * On LCD, cmm and cm blink
@@ -10,7 +9,7 @@ LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, 
 const int TRIG = 9;
 const int ECHO = 10;
 long duration;
-int cm, distanceInch;
+int cm;
 
 //for button setup-------
 const int buttonPinLength = 12;
@@ -25,7 +24,7 @@ int Area_buttonState = 0;
 int room_length = 0;
 int room_width = 0;
 //----------------------
-byte newCharSquare[8] = {
+byte squared_special_char[8] = {
   B01100,
   B10010,
   B00100,
@@ -37,8 +36,8 @@ byte newCharSquare[8] = {
 };
 
 void setup() {
-  lcd.createChar(0, newCharSquare);
-  lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+  lcd.createChar(0, squared_special_char);
+  lcd.begin(16,2);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   //Serial.begin(9600);
@@ -54,7 +53,7 @@ void setup() {
 }
 
 void loop() {
-  lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.setCursor(0,0);
   //----------------------------Reset Button-------------------------------------------
   if((room_length == 0) && (room_width == 0)) {
     lcd.print("Ready to measure");
@@ -66,59 +65,56 @@ void loop() {
 }
 
 //=================================Length Measurement===================================
-Length_buttonState = digitalRead(buttonPinLength);
-if (Length_buttonState == LOW) {
-   digitalWrite(TRIG, LOW);
-   delayMicroseconds(2);
-   digitalWrite(TRIG, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(TRIG, LOW);
-
-   duration = pulseIn(ECHO, HIGH);
-   room_length = (duration/2) * 0.0343;
-   lcd.clear();
-   lcd.print("Length: "); // Prints string "Distance" on the LCD
-   lcd.print(room_length); // Prints the distance value from the sensor
-   lcd.print("  cm");
-   delay(10);
-   //room_length = cm;
-}
-//Serial.println("outside ELSE condition");
+   Length_buttonState = digitalRead(buttonPinLength);
+  if (Length_buttonState == LOW) {
+     digitalWrite(TRIG, LOW);
+     delayMicroseconds(2);
+     digitalWrite(TRIG, HIGH);
+     delayMicroseconds(10);
+     digitalWrite(TRIG, LOW);
+  
+     duration = pulseIn(ECHO, HIGH);
+     room_length = (duration/2) * 0.0343;
+     lcd.clear();
+     lcd.print("Length: ");
+     lcd.print(room_length);
+     lcd.print("  cm");
+     delay(10);
+  }
 
 //=========================================================================================
 
 //=================================Width Measurement===================================
 
-Width_buttonState = digitalRead(buttonPinWidth);
-if (Width_buttonState == LOW){
-   digitalWrite(TRIG, LOW);
-   delayMicroseconds(2);
-   digitalWrite(TRIG, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(TRIG, LOW);
-
-   duration = pulseIn(ECHO, HIGH);
-   room_width = (duration/2) * 0.0343;
-  //distanceInch = duration*0.0133/2;
-   lcd.clear();
-   lcd.print("Width: "); // Prints string "Distance" on the LCD
-   lcd.print(room_width); // Prints the distance value from the sensor
-   lcd.print("  cm");
-   delay(10);
-}
+    Width_buttonState = digitalRead(buttonPinWidth);
+    if (Width_buttonState == LOW){
+       digitalWrite(TRIG, LOW);
+       delayMicroseconds(2);
+       digitalWrite(TRIG, HIGH);
+       delayMicroseconds(10);
+       digitalWrite(TRIG, LOW);
+    
+       duration = pulseIn(ECHO, HIGH);
+       room_width = (duration/2) * 0.0343;
+       lcd.clear();
+       lcd.print("Width: ");
+       lcd.print(room_width); 
+       lcd.print("  cm");
+       delay(10);
+    }
 //============================Area Calculation=========================================================
-Area_buttonState = digitalRead(buttonPinArea);
-if (Area_buttonState == LOW) {
-  int area = room_length * room_width;
-  //Preparing the display:
-  String area_to_str = String(area);
-  int area_string_length = area_to_str.length();
-  int cell_number = 9 + area_string_length;
-  lcd.clear();
-  lcd.print("Area: ");
-  lcd.print(area);
-  lcd.print(" cm");
-  lcd.setCursor(cell_number,0);
-  lcd.write((byte)0);
-}
+  Area_buttonState = digitalRead(buttonPinArea);
+  if (Area_buttonState == LOW) {
+    int area = room_length * room_width;
+    //Preparing the display:
+    String area_to_str = String(area);
+    int area_string_length = area_to_str.length();
+    int cell_number = 9 + area_string_length;
+    lcd.clear();
+    lcd.print("Area: ");
+    lcd.print(area);
+    lcd.print(" cm");
+    lcd.setCursor(cell_number,0);
+    lcd.write((byte)0);
+  }
 }
